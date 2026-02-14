a58cac55db664a14967359653e8610f050bb1e01
nordsoft <nordsoft@rambler.ru>
2026-02-14 16:54:12 +0100

    Auto-create script templates

--- addons/composition/plugin.gd
+++ addons/composition/plugin.gd
@@ -4,6 +4,32 @@ extends EditorPlugin
 
 var plugin
 
+const script_templates = [
+	"default"
+]
+const ProjectSettingsTemplatesPath = "editor/script/templates_search_path"
+const ScriptTemplateComponent = "/Component"
+const AddonScriptTemplates = "res://addons/composition/script_templates"
+
+func _enable_plugin() -> void:
+	var templates_path = ProjectSettings.get_setting(ProjectSettingsTemplatesPath)
+	if templates_path:
+		if DirAccess.make_dir_recursive_absolute(templates_path+ScriptTemplateComponent) == OK:
+			for script_template in script_templates:
+				var source = AddonScriptTemplates+ScriptTemplateComponent+"/"+script_template+".txt"
+				var destination = templates_path+ScriptTemplateComponent+"/"+script_template+".gd"
+				if DirAccess.copy_absolute(source, destination) == OK:
+					print_rich("[color=cyan][b]COMPOSITION:[/b] Script template added: ", destination, "[/color]")
+
+func _disable_plugin() -> void:
+	var templates_path = ProjectSettings.get_setting(ProjectSettingsTemplatesPath)
+	if templates_path:
+		for script_template in script_templates:
+			var target = templates_path+ScriptTemplateComponent+"/"+script_template+".gd"
+			if DirAccess.remove_absolute(target) == OK:
+				print_rich("[color=cyan][b]COMPOSITION:[/b] Script template removed: ", target, "[/color]")
+				EditorInterface.get_resource_filesystem().update_file(target)
+		DirAccess.remove_absolute(templates_path+ScriptTemplateComponent)
 
 func _enter_tree():
 	var ver_info: Dictionary = Engine.get_version_info()
